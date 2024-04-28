72b850f0fa9171feaed48bb810a2c97c9d9221e9
olicrea <olimar.digital@gmail.com>
2024-04-28 14:49:33 -0400

    la HU3 está casi lista, falta ponerle límite a la búsqueda de trajectorias para un solo día y no todos los días a partir del indicado con gte

--- src/controller/trajectories.ts
+++ src/controller/trajectories.ts
@@ -5,13 +5,18 @@ const prisma = new PrismaClient();
 
 export const historialTaxi = async (req: Request, resp: Response) => {
   try {
+    // id y date del req se convierten a los tipos correctos
     const id = Number(req.params.id);
-    const date = req.params.date as string;
+    const day = new Date(req.params.day); // sumar n día para usar con lte: menor que 
+    console.log('id:', id);
+    console.log('day:', day)
 
-    const historialTrajectories = await prisma.trajectories.findMany({
+    const historial = await prisma.trajectories.findMany({
       where: {
         taxi_id: id,
-        date: { gte: date },
+        date: { gte: day }, // greater than or equal to” (mayor que o igual a)
+        // usar lte para establecer límite de búsqueda: https://www.prisma.io/docs/orm/reference/prisma-client-reference#get-all-post-records-where-date_created-is-greater-than-march-19th-2020
+        // podría no usar lte y gte?
       },
       orderBy: {
         date: 'asc',
@@ -24,8 +29,10 @@ export const historialTaxi = async (req: Request, resp: Response) => {
       take: 10,
       skip: 20,
     });
-    resp.status(200).json(historialTrajectories);
+
+    resp.status(200).json(historial);
   } catch (error) {
+    console.error(error);
     return resp.status(500).send("Error getting taxi's locations");
   } finally {
     // Desconexión de Prisma
