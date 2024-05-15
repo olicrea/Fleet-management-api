d50ffa555cbdfcfa1a0c939768b6d8f74b11a983
olicrea <olimar.digital@gmail.com>
2024-05-15 10:09:49 -0400

    se define objeto para swagger

---
+++ src/swagger.ts
@@ -0,0 +1,41 @@
+import { SwaggerDefinition } from "swagger-jsdoc";
+
+const swaggerDefinition: SwaggerDefinition = {
+  openapi: '3.0.3',
+  info: {
+    title: 'Fleet Management API',
+    description: 'API para gestionar información de taxis con dispositivos GPS. [Más información.](https://github.com/olicrea/DEV013-fleet-management-api)',
+    version: '1.0.0',
+  },
+  servers: [
+    {
+      url: 'http://localhost:3001',
+    },
+  ],
+  components: {
+    schemas: {
+      Taxi: {
+        type: 'object',
+        properties: {
+          id: { type: 'integer' },
+          plate: { type: 'string' },
+          trajectories: {
+            type: 'array',
+          },
+        },
+      },
+      Trajectory: {
+        type: 'object',
+        properties: {
+          id: { type: 'integer' },
+          taxi_id: { type: 'integer' },
+          date: { type: 'string', format: 'date-time' },
+          latitude: { type: 'number' },
+          longitude: { type: 'number' },
+        },
+      },
+    },
+  },
+};
+
+export { swaggerDefinition };
