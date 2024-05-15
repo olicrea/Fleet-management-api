80382f5fa3eff5830c65cf649f20237f37e2460d
olicrea <olimar.digital@gmail.com>
2024-05-15 10:10:39 -0400

    modifico endpoints

--- src/controller/trajectories.ts
+++ src/controller/trajectories.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from 'express';
 
 const prisma = new PrismaClient();
 // https://www.prisma.io/docs/orm/prisma-client/queries/crud#read
+
+let skipHistorial = 0; // Variable para rastrear cuántos registros se han omitido
 export const historialTaxi = async (req: Request, resp: Response) => {
   try {
     // id y date del req se convierten a los tipos correctos
@@ -28,18 +30,23 @@ export const historialTaxi = async (req: Request, resp: Response) => {
         date: true,
       },
       take: 10,
-      skip: 20,
+      skip: skipHistorial,
     });
 
-    resp.status(200).json(historial);
+    // Incrementa el valor de "skip" en 10 para la próxima solicitud
+    skipHistorial += 10;
+
+    // Invierte el orden de los elementos en el array de historial
+    const historialInvertido = historial.reverse();
+
+    resp.status(200).json(historialInvertido);
   } catch (error) {
     console.error(error);
     return resp.status(500).send("Error getting taxi's locations");
-  } finally {
-    // Desconexión de Prisma
-    await prisma.$disconnect();
-}};
-
+  } 
+};
+// select da más control que include(porque traería todas las propiedades de la tabla relacionada)
+let skipLocation = 0;
 export const lastLocation = async (req: Request, resp: Response) => {
   try {
     const locations = await prisma.trajectories.findMany({
@@ -58,16 +65,15 @@ export const lastLocation = async (req: Request, resp: Response) => {
         date: true,
       },
       distinct: ['taxi_id'],
+      take: 10,
+      skip: skipLocation,
     });
+
+    skipHistorial += 10;
 
     resp.status(200).json(locations);
   } catch (error) {
     console.error(error);
     return resp.status(500).send("Error getting taxies's last locations");
-  } finally {
-    await prisma.$disconnect();
-  }
-
-
-
-}
+  } 
+};
